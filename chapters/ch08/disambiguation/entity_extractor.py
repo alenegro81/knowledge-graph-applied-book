0f47b514ec4f531c2975286533189bb2b4140214
Giuseppe Futia <giuseppe.futia@graphaware.com>
2024-02-15 08:53:32 +0100

    Extract entities based on threshold + minor

--- chapters/ch08/disambiguation/entity_extractor.py
+++ chapters/ch08/disambiguation/entity_extractor.py
@@ -6,12 +6,11 @@ class EntityExtractor:
        scispacy linker to UMLS concepts"""
     def __init__(self, metathesaurus_file):
         self.types = {}
-
+        print('scispacy model loading...')
         self.nlp = spacy.load("en_core_sci_md")
         self.nlp.add_pipe("scispacy_linker", config={"resolve_abbreviations": True, "linker_name": "umls"})
         self.linker = self.nlp.get_pipe("scispacy_linker")
-        self.linker_dict = self.linker.kb.cui_to_entity
-
+        self.linker_dict = self.linker.kb.cui_to_entity      
         self.process_metathesaurus(metathesaurus_file)
 
     def process_metathesaurus(self, metathesaurus_file):
@@ -19,7 +18,7 @@ class EntityExtractor:
            Create a mapping from CuIDs returned by scispacy linker to a descriptive string"""
         for line in metathesaurus_file.open():
             values = line.split('|')
-            self.types[values[1]] = values[3]
+            self.types[values[2]] = values[3].strip()
 
     def create_entity_dict(self, doc, ent):
         """collect the data from entity object from scispacy into structured dictionary"""
@@ -46,6 +45,9 @@ class EntityExtractor:
                 'confidence': umls_ent[1]
             })
 
+        if ent_dict['ned'][0]['confidence'] < 0.8:
+            return
+
         ent_dict['selected_ned_id'] = ent_dict['ned'][0]['id']
         ent_dict['selected_ned_name'] = ent_dict['ned'][0]['name']
         ent_dict['selected_ned_definition'] = ent_dict['ned'][0]['definition']
@@ -59,5 +61,10 @@ class EntityExtractor:
     def extract_ents(self, text):
         """convert text into a structured dictionary of entities"""
         doc = self.nlp(text)
-        ents = [self.create_entity_dict(doc, ent) for ent in doc.ents if len(ent._.kb_ents) != 0]
+        ents = []
+        for e in doc.ents:
+            ent = self.create_entity_dict(doc, e)
+            if ent is not None:
+                ents.append(ent)
+
         return ents
