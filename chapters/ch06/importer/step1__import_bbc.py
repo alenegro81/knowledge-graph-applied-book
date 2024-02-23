27c08a1e8667cd10c33e3ec1805285f005bcbc8c
fxiii <fxiii@gitlab.com>
2024-02-23 18:01:16 +0100

    stream documents, split extract keywords "importer"

--- chapters/ch06/importer/step1__import_bbc.py
+++ chapters/ch06/importer/step1__import_bbc.py
@@ -1,13 +1,11 @@
-import os
 import sys
+from pathlib import Path
+
 import math
 import spacy
-import pytextrank
-from pathlib import Path
-from neo4j import GraphDatabase, basic_auth
+from neo4j.exceptions import ClientError as Neo4jClientError
 
 from util.base_importer import BaseImporter
-from util.graphdb_base import GraphDBBase
 
 
 class BBCImporter(BaseImporter):
@@ -24,88 +22,29 @@ class BBCImporter(BaseImporter):
         # CREATE DATABASE news
         self.create_indices()
 
-        self.QUERY_IMPORT = """MERGE (t:Topic {name: $topic})
-            WITH t
-
-            UNWIND $inputs AS doc
-
-            MERGE (n:Document {id: doc.id}) 
-            SET n.title = doc.title, n.text = doc.text
-
-            MERGE (t)-[:HAS_DOCUMENT]->(n)
-            """
-
-        self.QUERY_ENTITIES = """UNWIND $inputs AS doc
-
-            MATCH (n:Document {id: doc.id})
-
-            FOREACH(entity IN doc.PERSON |
-              MERGE (e:Person {name: entity})
-              MERGE (n)-[:MENTIONS_PERSON]->(e)
-            )
-
-            FOREACH(entity IN doc.ORG |
-              MERGE (e:Organization {name: entity})
-              MERGE (n)-[:MENTIONS_ORGANIZATION]->(e)
-            )
-
-            FOREACH(entity IN doc.GPE |
-              MERGE (e:Location {name: entity})
-              MERGE (n)-[:MENTIONS_LOCATION]->(e)
-            )
-
-            FOREACH(entity IN doc.DATE |
-              MERGE (e:Date {name: toLower(entity)})
-              MERGE (n)-[:MENTIONS_DATE]->(e)
-            )
-
-            FOREACH(entity IN doc.MONEY |
-              MERGE (e:Money {name: entity})
-              MERGE (n)-[:MENTIONS_MONEY]->(e)
-            )
-
-            FOREACH(entity IN doc.CARDINAL |
-              MERGE (e:Number {name: entity})
-              MERGE (n)-[:MENTIONS_NUMER]->(e)
-            )
-
-            FOREACH(entity IN doc.NORP |
-              MERGE (e:NatReligPolitGroup {name: entity})
-              MERGE (n)-[:MENTIONS_GROUP]->(e)
-            )
-
-            FOREACH(entity IN doc.WORK_OF_ART |
-              MERGE (e:WorkOfArt {name: entity})
-              MERGE (n)-[:MENTIONS_WORK_OF_ART]->(e)
-            )
-            """
-
-        self.QUERY_STORE_KEYWORDS = """UNWIND $inputs AS doc
-
-              MATCH (n:Document)
-              WHERE id(n) = doc.id
-
-              FOREACH(entity IN doc.keywords |
-                MERGE (e:Keyword {name: entity.name})
-                MERGE (n)-[r:MENTIONS_KEYWORD]->(e)
-                SET r.rank = entity.rank
-              )
-            """
-
     def create_indices(self):
         with self._driver.session(database=self._database) as session:
-            session.run("CREATE CONSTRAINT doc ON (n:Document) ASSERT n.id IS UNIQUE")
-            session.run("CREATE CONSTRAINT person ON (n:Person) ASSERT n.name IS UNIQUE")
-            session.run("CREATE CONSTRAINT location ON (n:Location) ASSERT n.name IS UNIQUE")
-            session.run("CREATE CONSTRAINT organization ON (n:Organization) ASSERT n.name IS UNIQUE")
-            session.run("CREATE CONSTRAINT date ON (n:Date) ASSERT n.name IS UNIQUE")
-            session.run("CREATE CONSTRAINT money ON (n:Money) ASSERT n.name IS UNIQUE")
-            session.run("CREATE CONSTRAINT number ON (n:Number) ASSERT n.name IS UNIQUE")
-            session.run("CREATE CONSTRAINT group ON (n:NatReligPolitGroup) ASSERT n.name IS UNIQUE")
-            session.run("CREATE CONSTRAINT workofart ON (n:WorkOfArt) ASSERT n.name IS UNIQUE")
-            session.run("CREATE CONSTRAINT keyword ON (n:Keyword) ASSERT n.name IS UNIQUE")
+            query = """
+            CREATE CONSTRAINT doc_id_unique IF NOT EXISTS FOR (n:Document) REQUIRE n.id IS UNIQUE;
+            CREATE CONSTRAINT person_name_unique IF NOT EXISTS FOR (n:Person) REQUIRE n.name IS UNIQUE;
+            CREATE CONSTRAINT location_name_unique IF NOT EXISTS FOR (n:Location) REQUIRE n.name IS UNIQUE;
+            CREATE CONSTRAINT organization_name_unique IF NOT EXISTS FOR (n:Organization) REQUIRE n.name IS UNIQUE;
+            CREATE CONSTRAINT date_name_unique IF NOT EXISTS FOR (n:Date) REQUIRE n.name IS UNIQUE;
+            CREATE CONSTRAINT money_name_unique IF NOT EXISTS FOR (n:Money) REQUIRE n.name IS UNIQUE;
+            CREATE CONSTRAINT number_name_unique IF NOT EXISTS FOR (n:Number) REQUIRE n.name IS UNIQUE;
+            CREATE CONSTRAINT group_name_unique IF NOT EXISTS FOR (n:NatReligPolitGroup) REQUIRE n.name IS UNIQUE;
+            CREATE CONSTRAINT workofart_name_unique IF NOT EXISTS FOR (n:WorkOfArt) REQUIRE n.name IS UNIQUE;
+            CREATE CONSTRAINT keyword_name_unique IF NOT EXISTS FOR (n:Keyword) REQUIRE n.name IS UNIQUE"""
+            for q in query.split(";"):
+                try:
+                    session.run(q)
+                except Neo4jClientError as e:
+                    # ignore if we already have the rule in place
+                    if e.code != "Neo.ClientError.Schema.EquivalentSchemaRuleAlreadyExists":
+                        raise e
 
-    def cleanse_entity(self, en: str):
+    @staticmethod
+    def cleanse_entity(en: str):
         TO_IGNORE = ["the", "a"]
         tokenized = en.split()
         if tokenized[0].lower() in TO_IGNORE:
@@ -113,74 +52,135 @@ class BBCImporter(BaseImporter):
         en = en.replace("\'s", "")
         return en.strip()
 
-    def ingest_documents(self, data_path: Path):
-        data = {x: list() for x in os.listdir(data_path) if os.path.isdir(os.path.join(data_path, x))}
-        for topic in data.keys():
-            files = os.listdir(os.path.join(data_path, topic))
-            print(f"Ingesting {len(files)} files from {topic} ...")
-            for file in files:
-                id = topic + "_" + file[:-4]
-                with open(os.path.join(data_path, topic, file), 'r') as f:
-                    try:
-                        lines = [x.strip() for x in f.readlines() if len(x.strip()) > 1]
-                        data[topic].append({'id': id, 'title': lines[0], 'text': "\n".join(lines[1:])})
-                    except UnicodeDecodeError:
-                        print(f"UnicodeDecodeError for {id}")
-        return data
+    @staticmethod
+    def count_documents(data_path: Path):
+        return sum(1 for _ in data_path.glob("*/*.txt"))
 
-    def enrich_by_nlp(self, documents: list):
-        for doc in documents:
-            processed = self.nlp(doc['title'] + ".\n\n" + doc['text'])
-            for en in processed.ents:
-                if en.label_ not in doc:
-                    doc[en.label_] = list()
-                doc[en.label_].append(self.cleanse_entity(en.text))
+    def get_documents(self, data_path: Path):
+        for file in data_path.glob("*/*.txt"):
+            # file: xyz/topic1/123.txt => id: topic1_123
+            id_ = f"{file.parent.name}_{file.name[:-4]}"
+            with file.open('r') as f:
+                try:
+                    lines = [x.strip() for x in f if len(x.strip()) > 1]
+                    document = {"topic": file.parent.name, "id": id_, "title": lines[0],
+                                "text": "\n".join(lines[1:])}
+                    self.enrich_document(document)
+                    yield document
+                except UnicodeDecodeError:
+                    print(f"UnicodeDecodeError for {id}")
 
-    def run(self, data_path: Path):
-        # Ingest docments and run NLP processing
-        with self._driver.session(database=self._database) as session:
-            inputs = self.ingest_documents(data_path)
-            for topic in inputs.keys():
-                print(f"Enriching {topic} documents")
-                self.enrich_by_nlp(inputs[topic])
-                session.run(self.QUERY_IMPORT, topic=topic, inputs=inputs[topic])
-                session.run(self.QUERY_ENTITIES, inputs=inputs[topic])
+    def enrich_document(self, document: dict):
+        processed = self.nlp(document['title'] + ".\n\n" + document['text'])
+        for en in processed.ents:
+            if en.label_ not in document:
+                document[en.label_] = list()
+            document[en.label_].append(self.cleanse_entity(en.text))
 
-    def extract_keywords(self):
-        QUERY_INGEST = """MATCH (n:Document)
-              RETURN id(n) AS id, n.title AS title, n.text AS text
-            """
+    def import_documents(self, data_path: Path):
+        import_document_query = """
+        UNWIND $batch as item
+        MERGE (t:Topic {name: item.topic})
+        
+        WITH item,t
+        MERGE (n:Document {id: item.id}) 
+        SET n.title = item.title, n.text = item.text
+        MERGE (t)-[:HAS_DOCUMENT]->(n)
+        
+        WITH item,t,n
+        FOREACH(entity IN item.PERSON |
+              MERGE (e:Person {name: entity})
+              MERGE (n)-[:MENTIONS_PERSON]->(e)
+            )
 
-        def cleanse_keyword(kw: str):
-            TO_IGNORE = ["the", "their", "a"]
-            tokenized = kw.split()
-            if tokenized[0].lower() in TO_IGNORE:
-                kw = kw[len(tokenized[0]):]
-            return kw.lower().strip()
+            FOREACH(entity IN item.ORG |
+              MERGE (e:Organization {name: entity})
+              MERGE (n)-[:MENTIONS_ORGANIZATION]->(e)
+            )
 
+            FOREACH(entity IN item.GPE |
+              MERGE (e:Location {name: entity})
+              MERGE (n)-[:MENTIONS_LOCATION]->(e)
+            )
+
+            FOREACH(entity IN item.DATE |
+              MERGE (e:Date {name: toLower(entity)})
+              MERGE (n)-[:MENTIONS_DATE]->(e)
+            )
+
+            FOREACH(entity IN item.MONEY |
+              MERGE (e:Money {name: entity})
+              MERGE (n)-[:MENTIONS_MONEY]->(e)
+            )
+
+            FOREACH(entity IN item.CARDINAL |
+              MERGE (e:Number {name: entity})
+              MERGE (n)-[:MENTIONS_NUMER]->(e)
+            )
+
+            FOREACH(entity IN item.NORP |
+              MERGE (e:NatReligPolitGroup {name: entity})
+              MERGE (n)-[:MENTIONS_GROUP]->(e)
+            )
+
+            FOREACH(entity IN item.WORK_OF_ART |
+              MERGE (e:WorkOfArt {name: entity})
+              MERGE (n)-[:MENTIONS_WORK_OF_ART]->(e)
+            )
+        """
+        size = self.count_documents(data_path)
+        self.batch_size = 50
+        self.batch_store(import_document_query, self.get_documents(data_path), size=size, desc="importing documents")
+
+
+class BBCKeywordImporter(BaseImporter):
+    def __init__(self, argv):
+        super().__init__(command=__file__, argv=argv)
+        self._database = "news"
+        self.nlp = spacy.load("en_core_web_sm")
         # add keyword extraction to the NLP pipeline
         self.nlp.add_pipe("textrank")
 
-        # get data for keyword extraction
+    @staticmethod
+    def cleanse_keyword(kw: str):
+        TO_IGNORE = ["the", "their", "a"]
+        tokenized = kw.split()
+        if tokenized[0].lower() in TO_IGNORE:
+            kw = kw[len(tokenized[0]):]
+        return kw.lower().strip()
+
+    def count_documents(self):
+        count_query = "MATCH (n:Document) RETURN count(n) as total"
         with self._driver.session(database=self._database) as session:
-            documents = session.run(QUERY_INGEST).data()
-        print(f"Retrieved {len(documents)} documents")
+            return session.run(count_query).single()["total"]
 
-        NUM_BATCHES = 200
-        batches = spacy.util.minibatch(documents, NUM_BATCHES)
-        print(f"Dataset split into {math.ceil(1.0 * len(documents) / NUM_BATCHES)} batches")
-        for idx, batch in enumerate(batches):
-            print(f"Processing batch {idx}")
-            # extract keywords
-            for doc in batch:
-                processed = self.nlp(doc['title'] + ".\n\n" + doc['text'])
-                doc['keywords'] = [{'name': cleanse_keyword(x.text), 'rank': x.rank} for x in processed._.phrases if
-                                   len(x.text) > 1][:30]
+    def get_documents(self):
+        query_documents = """
+        MATCH (n:Document)
+        RETURN id(n) AS id, n.title AS title, n.text AS text
+        """
+        with self._driver.session(database=self._database) as session:
+            for document in session.run(query_documents):
+                document = dict(document)
+                processed = self.nlp(document['title'] + ".\n\n" + document['text'])
+                document['keywords'] = [{'name': self.cleanse_keyword(x.text), 'rank': x.rank}
+                                        for x in processed._.phrases
+                                        if len(x.text) > 1][:30]
+                yield document
 
-            # store back to Neo4j
-            with self._driver.session(database=self._database) as session:
-                print("Storing a batch of keywords to Neo4j")
-                session.run(self.QUERY_STORE_KEYWORDS, inputs=batch)
+    def import_keyword(self):
+        query_store_keywords = """
+        UNWIND $batch AS item
+        MATCH (n:Document)
+        WHERE id(n) = item.id
+        FOREACH(entity IN item.keywords |
+            MERGE (e:Keyword {name: entity.name})
+            MERGE (n)-[r:MENTIONS_KEYWORD]->(e)
+            SET r.rank = entity.rank
+        )"""
+        size = self.count_documents()
+        self.batch_size = 50
+        self.batch_store(query_store_keywords, self.get_documents(), size=size, desc="importing keywords")
 
 
 if __name__ == "__main__":
@@ -194,7 +194,8 @@ if __name__ == "__main__":
     base_path = Path(base_path)
 
     # Ingest articles & run NLP processing
-    importer.run(base_path)
+    importer.import_documents(base_path)
 
+    importerKW = BBCKeywordImporter(argv=sys.argv[1:])
     # for each article, extract keywords and key-phrases (enrichment of the KG created in previous step)
-    importer.extract_keywords()
+    importerKW.import_keyword()
