70c1f3eecfd1dd1343fbdaef70fe8d23246bb66c
Vlasta Kus <vlastimil.kus@gmail.com>
2025-03-21 18:57:12 +0100

    Vlasta updates ch07 and added ch15. (#7)
    * Prompt and code updates.
    
    * Added ch15 listings.
    
    * Added initial code for listing 15.5.
    
    * new code
    
    * code updates
    
    * Final improvements.

--- chapters/ch07/listings/listing_5.py
+++ chapters/ch07/listings/listing_5.py
@@ -4,7 +4,7 @@ prompt_segments['task'] = """You are an expert on constructing Knowledge Graphs 
 Given a prompt, identify as many entities and relations among them as possible and output a list of relations in the format [ENTITY 1, ENTITY 1 TYPE, RELATION, ENTITY 2, ENTITY 2 TYPE]. 
 The relations are directed, so the order matters.
 
-Entities of interest: person, location, organization, date, occupation (a.k.a. person's work, specialization, research discipline, interests, occupation).
+Entities of interest: person, location, organization, date, occupation (a.k.a. person's work, specialization, research discipline, interests, occupation, technology).
 Top relations of interest: "works for", "works with", "student of" (link students with their teachers/advisors), "talked about" (a person talking about another person), "talked with" (a person talking with another person), "works on" (assignment of persons to their occupation, work, specialisation, research discipline, interests etc.).
 
 Note that persons are often first referenced by their full name, and then mentioned only by their surname or initials, for example: "A. N. Richards" becomes "Richards", "ANR", or just "R.".
